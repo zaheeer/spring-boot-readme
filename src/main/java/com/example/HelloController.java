f641ac2e684f60308f8185a7f9107c5351c3710f
Zahirabbas <zaheershaikh14@gmail.com>
2024-04-29 16:29:43 +0530

    Test Commit

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,8 +1,14 @@
 package com.example;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -15,10 +21,24 @@ public class HelloController {
 
     private final String JSON_PLACEHOLDER_URL = "https://jsonplaceholder.typicode.com/posts";
 
+
     @GetMapping("/posts")
     public String getPostFromJSONPlaceholder() {
         RestTemplate restTemplate = new RestTemplate();
         String post = restTemplate.getForObject(JSON_PLACEHOLDER_URL, String.class);
         return post;
+    }
+
+    @PostMapping("/posts")
+    public ResponseEntity<String> createPost(@RequestBody PostRequestBody postRequestBody) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<PostRequestBody> requestEntity = new HttpEntity<>(postRequestBody, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity("https://jsonplaceholder.typicode.com/posts", requestEntity, String.class);
+
+        return ResponseEntity.ok(responseEntity.getBody());
     }
 }
---
+++ src/main/java/com/example/PostRequestBody.java
@@ -0,0 +1,42 @@
+package com.example;
+
+public class PostRequestBody {
+    private Long id;
+    private String title;
+    private String body;
+    private Long userId;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
